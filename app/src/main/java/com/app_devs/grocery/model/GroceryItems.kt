ebc2c9db47630d62d20b73edc33aab02f231d9d0
Sumit Kushwaha <kushwahasumit024@gmail.com>
2021-09-11 20:02:48 +0530

    Done and dusted

--- app/src/main/java/com/app_devs/grocery/model/GroceryItems.kt
+++ app/src/main/java/com/app_devs/grocery/model/GroceryItems.kt
@@ -3,7 +3,7 @@ package com.app_devs.grocery.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-//always make sure to have the id as integer/ long
+//always make sure to have the id as integer/ long. this was the error
 @Entity(tableName = "grocery_items")
 data class GroceryItems(
     @PrimaryKey(autoGenerate = true)
